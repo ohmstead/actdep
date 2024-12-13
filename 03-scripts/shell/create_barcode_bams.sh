b18ae581577998532db4a96072546bf81cafc983
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2024-12-13 15:45:23 -0800

    Edited create_barcode_bams.sh

--- 03-scripts/shell/create_barcode_bams.sh
+++ 03-scripts/shell/create_barcode_bams.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-input_bam="/Volumes/jack/seq/analysis_May2024/sublibrary1/process/barcode_headAligned_anno_sorted.bam"
-barcode_list="/Users/jack/Downloads/barcodes.txt"
+input_bam="/Volumes/jack/seq/analysis_May2024/sublibrary1/process/barcode_headAligned_anno_sorted_sublib1_cutoff80_dedup.bam"
+barcode_list="04-analysis/May2024_barcodes.txt"
 output_dir="/Volumes/jack/seq/analysis_May2024/sublibrary1/process/sc_bams"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-# Function to process a single barcode
-process_barcode() {
-    barcode=$1
-    echo "Processing barcode: $barcode"
-    samtools view -h "$input_bam" | grep -E "@|CB:Z:$barcode" | samtools view -bS - > "$output_dir/$barcode.bam"
-}
+# Count the total number of barcodes
+total_barcodes=$(wc -l < "$barcode_list")
+counter=0
 
-export -f process_barcode
-export input_bam
-export output_dir
-
-# Use GNU parallel to process barcodes concurrently
-parallel -j 8 process_barcode ::: $(cat $barcode_list)
+# Loop through each barcode in the barcode list
+while IFS= read -r barcode; do
+    printf "Processing barcode %s (%d/%d)\n" "$barcode" "$((counter + 1))" "$total_barcodes"
+    samtools view -h -d CB:$barcode "$input_bam" -@ 10 > "$output_dir/$barcode.bam"
+    counter=$((counter + 1))
+done < "$barcode_list"
