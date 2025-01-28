43763b71958cc4aa9a33be1ca40afc7de91f1ac6
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2025-01-28 10:04:12 -0800

    figure1 activation

--- 03-scripts/figures/figure1_activation_percent.R
+++ 03-scripts/figures/figure1_activation_percent.R
@@ -11,7 +11,7 @@ library(glue)
 
 source("03-scripts/R/seq_functions.R")
 nuclei <- LoadDataset("May2024", "combined")
-activity_colors <- LoadActivityColors("May2024")
+activity_colors <- LoadActivityColors("Dec2024")
 
 
 # ---- get active cells IEG ----
@@ -27,11 +27,11 @@ activation_thresholds_IEGs <- outputs$activation_thresholds
 # ---- plot % active IEGs ----
 print("Plotting percent of cells active using IEGs...")
 
-p_activation_IEG <- ggplot(df_active_cells_IEGs, aes(x = condition, y = num_upregd_genes, fill = condition)) +
-  geom_jitter(width = 0.3, height = 0.25, shape = 21, alpha = 0.5) +
+p_activation_IEG <- ggplot(df_active_cells_IEGs, aes(x = activity_condition, y = num_upregd_genes, fill = activity_condition)) +
+  geom_jitter(width = 0.3, height = 0.25, shape = 21, alpha = 0.3) +
   geom_boxplot(width = 0.3, alpha = 0.5, outlier.shape = NA) +
   geom_hline(yintercept = 2.5, linewidth = 0.5, linetype = 'dashed', color = 'black') +
-  scale_fill_manual(values = condition_colors) +
+  scale_fill_manual(values = activity_colors) +
   theme_minimal() +
   # make y-axis labels at 0, 3, 5, and 10
   scale_y_continuous(breaks = c(0, 3, 5, 10)) +
@@ -50,7 +50,7 @@ print("Getting percent of cells active using lncRNAs...")
 
 gene_biotypes <- read_csv("04-analysis/all_gene_stats.csv")
 
-lncRNA_degs <- read_csv("04-analysis/DEGs/condition/016_CA1-ProS_Glut_d30m_vs_dSE.csv") |> 
+lncRNA_degs <- read_csv("04-analysis/DEGs/May2024_activity_condition/016_CA1-ProS_Glut_EE30m_vs_SE.csv") |> 
   left_join(gene_biotypes, by = c('gene' = 'name')) |> 
   filter(biotype == 'lncRNA') |> 
   filter(chrom != 'mm39_X' & chrom != 'mm39_Y') |> 
@@ -64,11 +64,11 @@ activation_thresholds_lncRNA <- outputs$activation_thresholds
 # ---- plot % active lncRNA ----
 print("Plotting percent of cells active using lncRNAs...")
 
-p_activation_lncRNA <- ggplot(df_active_cells_lncRNA, aes(x = condition, y = num_upregd_genes, fill = condition)) +
+p_activation_lncRNA <- ggplot(df_active_cells_lncRNA, aes(x = activity_condition, y = num_upregd_genes, fill = activity_condition)) +
   geom_jitter(width = 0.3, height = 0.25, shape = 21, size = 1, alpha = 0.5) +
   geom_boxplot(width = 0.3, alpha = 0.5, outlier.shape = NA) +
   geom_hline(yintercept = 1.5, linewidth = 0.5, linetype = 'dashed', color = 'black') +
-  scale_fill_manual(values = condition_colors) +
+  scale_fill_manual(values = activity_colors) +
   theme_minimal() +
   scale_y_continuous(breaks = c(0, 2, 5, 10)) +
   coord_cartesian(ylim = c(0, 6)) +
@@ -85,14 +85,14 @@ print(lncRNA_degs$gene)
 print("Plotting KDE example insets...")
 
 p_SE <- df_active_cells_IEGs |> 
-  filter(condition == 'dSE') |> 
-  ggplot() +
-  geom_density(aes(x = Npas4, fill = condition)) +
+  filter(activity_condition == 'SE') |> 
+ggplot() +
+  geom_density(aes(x = Npas4, fill = activity_condition)) +
   geom_vline(xintercept = 0.5, color = 'black', linetype = 'dotted', lwd=1) +
   geom_label(x = 0.55, y = 2.5, label = '90th expression percentile in HC cells', size = 6, color = 'black', hjust = 0) +
   geom_label(x = 0.52, y = 1, label = "'active'", size = 4, color = "black", hjust = 0) +
   geom_label(x = 0.48, y = 1, label = "'inactive'", size = 4, color = "black", hjust = 1) +
-  scale_fill_manual(values = condition_colors) +
+  scale_fill_manual(values = activity_colors) +
   coord_cartesian(xlim = c(0, 5), expand = FALSE) +
   labs(title = "Npas4 in HC cells", x = "Npas4 expression") +
   theme_bw() +
@@ -101,14 +101,14 @@ p_SE <- df_active_cells_IEGs |>
   )
 
 p_EE_30m <- df_active_cells_IEGs |> 
-  filter(condition == 'd30m') |> 
+  filter(activity_condition == 'EE30m') |> 
   ggplot() +
-  geom_density(aes(x = Npas4, fill = condition)) +
+  geom_density(aes(x = Npas4, fill = activity_condition)) +
   geom_vline(xintercept = 0.5, color = 'black', linetype = 'dotted', lwd=1) +
   geom_label(x = 0.55, y = 1.5, label = '90th expression percentile in HC cells', size = 6, color = 'black', hjust = 0) +
   geom_label(x = 0.52, y = 0.75, label = "'active'", size = 4, color = "black", hjust = 0) +
   geom_label(x = 0.48, y = 0.75, label = "'inactive'", size = 4, color = "black", hjust = 1) +
-  scale_fill_manual(values = condition_colors) +
+  scale_fill_manual(values = activity_colors) +
   coord_cartesian(xlim = c(0, 5), expand = FALSE) +
   labs(title = "Npas4 in KA cells", x = "Npas4 expression") +
   theme_bw() +
@@ -117,14 +117,14 @@ p_EE_30m <- df_active_cells_IEGs |>
   )
 
 p_KA <- df_active_cells_IEGs |> 
-  filter(condition == 'KA') |> 
+  filter(activity_condition == 'KA') |> 
   ggplot() +
-  geom_density(aes(x = Npas4, fill = condition)) +
+  geom_density(aes(x = Npas4, fill = activity_condition)) +
   geom_vline(xintercept = 0.5, color = 'black', linetype = 'dotted', lwd=1) +
   geom_label(x = 0.55, y = 0.65, label = '90th expression percentile in HC cells', size = 6, color = 'black', hjust = 0) +
   geom_label(x = 0.52, y = 0.25, label = "'active'", size = 4, color = "black", hjust = 0) +
   geom_label(x = 0.48, y = 0.25, label = "'inactive'", size = 4, color = "black", hjust = 1) +
-  scale_fill_manual(values = condition_colors) +
+  scale_fill_manual(values = activity_colors) +
   coord_cartesian(xlim = c(0, 5), expand = FALSE) +
   labs(title = "Npas4 in 30m EE cells", x = "Npas4 expression") +
   theme_bw() +
@@ -139,24 +139,24 @@ print(p_SE / p_EE_30m / p_KA)
 if (SAVE_PLOTS) {
   print("Saving plots...")
   ggsave(plot = p_activation_IEG,
-         path = '05-results/figure_1/raw_R_plots/', 
+         path = '05-results/figure1/raw_R_plots/', 
          filename = 'activation_IEGs.png', 
          width = 7, height = 3, dpi = 900)
   ggsave(plot = p_activation_lncRNA,
-         path = '05-results/figure_1/raw_R_plots/', 
+         path = '05-results/figure1/raw_R_plots/', 
          filename = 'activation_lncRNAs.png', 
          width = 7, height = 3, dpi = 900)
   # inset plots
   ggsave(plot = p_SE, 
-         path = '05-results/figure_1/raw_R_plots/', 
+         path = '05-results/figure1/raw_R_plots/', 
          filename = 'HC_inset.png', 
          width = 8, height = 2.5, dpi = 900)
   ggsave(plot = p_EE_30m,
-         path = '05-results/figure_1/raw_R_plots/', 
+         path = '05-results/figure1/raw_R_plots/', 
          filename = 'EE_30m_inset.png', 
          width = 8, height = 2, dpi = 900)
   ggsave(plot = p_KA,
-         path = '05-results/figure_1/raw_R_plots/', 
+         path = '05-results/figure1/raw_R_plots/', 
          filename = 'KA_inset.png', 
          width = 8, height = 2.5, dpi = 900)
 } else {
