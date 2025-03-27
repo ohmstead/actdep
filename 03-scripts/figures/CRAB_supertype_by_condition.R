b214a42db13f178ebd05e8bac5e1ac70f67c4d91
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2025-03-27 16:39:50 -0700

    CRAB supertype by condition

--- 03-scripts/figures/CRAB_supertype_by_condition.R
+++ 03-scripts/figures/CRAB_supertype_by_condition.R
@@ -1,54 +1,48 @@
 # This script plots UMAPs for each gigaclass, colored by activity_condition, subclass, and supertype.
 # It also plots, for each subclass, the supertype distribution in each activity_condition.
-
-library(ggplot2)
-library(ggvenn)
-library(readr)
-library(dplyr)
-library(glue)
-library(patchwork)
-
 source('03-scripts/R/seq_functions.R')
 
+library(ggbreak)
+
+save_dir <- '05-results/CRAB/raw_R_plots/'
 nuclei <- LoadDataset('Dec2024')
+seurat_subsets <- LoadDataset('Dec2024', as_gigaclasses = T)
+
 activity_colors <- LoadActivityColors()
-
-
-# gigaclass UMAPs ------------------------------------------------
 subclass_colors <- LoadAllenColors('subclass')
 supertype_colors <- LoadAllenColors('supertype')
 cluster_colors <- LoadAllenColors('cluster')
-activity_colors <- LoadActivityColors()
 
+# gigaclass UMAPs ------------------------------------------------
+# remove low-N subclasses
 subclass_sets <- LoadSubclassesToUse(nuclei, as_gigaclasses = TRUE)
 subclass_sets$excitatory <- subclass_sets$excitatory[-2] # rm CA2
 subclass_sets$inhibitory <- subclass_sets$inhibitory[-c(2:6)] # rm low-count interneurons
 subclass_sets$glia <- subclass_sets$glia[-1] # rm IMNs
 
 # re-embed nuclei
-for (gigatype in names(subclass_sets)) {
-  nuclei_gigatype <- nuclei |> 
-    subset(subclass_name %in% subclass_sets[[gigatype]]) |> 
-    RunUMAP(dims = 1:41)
-  p1 <- DimPlot(nuclei_gigatype, group.by = 'subclass_name') +
+for (gigaclass in names(seurat_subsets)) {
+  nuclei_gigaclass <- seurat_subsets[[gigaclass]] |> 
+    RunUMAP(dims = 1:41, seed.use = 17)
+  p1 <- DimPlot(nuclei_gigaclass, group.by = 'subclass_name') +
     scale_color_manual(values = subclass_colors) +
     theme_void() +
     theme(legend.position = 'none',
           # panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
           plot.title = element_blank())
-  p2 <- DimPlot(nuclei_gigatype, group.by = 'activity_condition') +
+  p2 <- DimPlot(nuclei_gigaclass, group.by = 'activity_condition') +
     scale_color_manual(values = activity_colors) +
     theme_void() +
     theme(legend.position = 'none',
           # panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
           plot.title = element_blank())
-  p3 <- DimPlot(nuclei_gigatype, group.by = 'supertype_name') +
+  p3 <- DimPlot(nuclei_gigaclass, group.by = 'supertype_name') +
     scale_color_manual(values = supertype_colors) +
     theme_void() +
     theme(legend.position = 'none',
           # panel.border = element_rect(color = "black", fill = NA, linewidth = 1),
           plot.title = element_blank())
-  p4 <- DimPlot(nuclei_gigatype, group.by = 'cluster_name') +
+  p4 <- DimPlot(nuclei_gigaclass, group.by = 'cluster_name') +
     scale_color_manual(values = cluster_colors) +
     theme_void() +
     theme(legend.position = 'none',
@@ -59,34 +53,51 @@ for (gigatype in names(subclass_sets)) {
   133
   233
   "
-  print(p1+p2+p3 + plot_layout(design = design, widths = c(1,0.75,0.75)))
-  si(1500,1000,'png')
+  p_UMAPs <- p1+p2+p3 + plot_layout(design = design, widths = c(1,0.75,0.75))
+  print(p_UMAPs)
+  
+  if (SAVE_PLOTS) {
+    # png
+    ggsave(plot = p_UMAPs,
+           filename = glue('reembedded_UMAPs_{gigaclass}.png'),
+           path = save_dir,
+           width = 15, height = 10, dpi = 300, bg = 'white')
+    # svg
+    ggsave(plot = p_UMAPs,
+           filename = glue('reembedded_UMAPs_{gigaclass}.svg'),
+           path = save_dir,
+           width = 15, height = 10, bg = 'transparent')
+    
+  }
 }
 
 
 # make df_cluster_counts ------------------------------------------------
 df_cluster_counts <- nuclei@meta.data |> 
-  filter(subclass_name %in% LoadSubclassesToUse(nuclei)) |>
-  group_by(activity_condition, subclass_name, supertype_name) |>
-  count(cluster_name, name = 'cluster_count')
+  filter(subclass_name %in% LoadSubclassesToUse(nuclei)) |> 
+  group_by(activity_condition, subclass_name, supertype_name) |> 
+  dplyr::count(cluster_name, name = 'cluster_count') |> 
+  group_by(activity_condition, subclass_name) |> 
+  print()
 
 
 # supertype by condition ------------------------------------------------
 # excitatory
-df_cluster_counts |> 
+p1 <- df_cluster_counts |> 
   filter(subclass_name %in% subclass_sets$excitatory) |> 
   group_by(activity_condition, subclass_name, supertype_name) |> 
   summarize(supertype_count = sum(cluster_count)) |> 
 ggplot() +
   aes(x = activity_condition, y = supertype_count, fill = supertype_name) +
   geom_bar(stat = "identity", position = 'fill', linewidth = 1) +
+  scale_fill_manual(values = supertype_colors) +
+  labs(x = '', y = 'Supertype proportion') +
   facet_wrap(~subclass_name, nrow = 1) +
-  scale_fill_manual(values = supertype_colors)
-  theme_void() +
-  theme(legend.position = 'none',
-        strip.text = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank())
+  theme(
+    axis.text.x = element_text(angle=30,hjust=1)
+  )
+print(p1)
+ggplotly(p1)
 
 # inhibitory
 df_cluster_counts |> 
@@ -97,12 +108,7 @@ ggplot() +
   aes(x = activity_condition, y = supertype_count, fill = supertype_name) +
   geom_bar(stat = "identity", position = 'fill', linewidth = 1) +
   facet_wrap(~subclass_name, nrow = 1) +
-  scale_fill_manual(values = supertype_colors) +
-  theme_void() +
-  theme(legend.position = 'none',
-        strip.text = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank())
+  scale_fill_manual(values = supertype_colors)
 
 # glia
 df_cluster_counts |> 
@@ -123,18 +129,15 @@ ggplot() +
 
 # cluster by condition ------------------------------------------------
 # excitatory
-df_cluster_counts |> 
+p2 <- df_cluster_counts |> 
   filter(subclass_name %in% subclass_sets$excitatory) |> 
 ggplot() +
   aes(x = activity_condition, y = cluster_count, fill = cluster_name) +
   geom_bar(stat = "identity", position = 'fill', linewidth = 1) +
   facet_wrap(~subclass_name, nrow = 1) +
-  scale_fill_manual(values = cluster_colors) +
-  theme_void() +
-  theme(legend.position = 'none',
-        strip.text = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank())
+  scale_fill_manual(values = cluster_colors)
+print(p2)
+ggplotly(p2)
 
 # inhibitory
 df_cluster_counts |> 
@@ -143,12 +146,7 @@ ggplot() +
   aes(x = activity_condition, y = cluster_count, fill = cluster_name) +
   geom_bar(stat = "identity", position = 'fill', linewidth = 1) +
   facet_wrap(~subclass_name, nrow = 1) +
-  scale_fill_manual(values = cluster_colors) +
-  theme_void() +
-  theme(legend.position = 'none',
-        strip.text = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank())
+  scale_fill_manual(values = cluster_colors)
 
 # glia
 df_cluster_counts |> 
@@ -157,30 +155,57 @@ ggplot() +
   aes(x = activity_condition, y = cluster_count, fill = cluster_name) +
   geom_bar(stat = "identity", position = 'fill', linewidth = 1) +
   facet_wrap(~subclass_name, nrow = 1) +
-  scale_fill_manual(values = cluster_colors) +
-  theme_void() +
-  theme(legend.position = 'none',
-        strip.text = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank())
+  scale_fill_manual(values = cluster_colors)
 
 
-# DG clusters by condition ------------------------------------------------
-df_cluster_counts |> 
-  filter(subclass_name == '037 DG Glut') |>
+# DG specifically ------------------------------------------------
+p3 <- df_cluster_counts |> 
+  ungroup() |> 
+  filter(subclass_name == '037 DG Glut') |> 
+  mutate(cluster_pct = cluster_count / sum(cluster_count) * 100, 
+         .by = c(activity_condition, subclass_name)) |>
+  filter(supertype_name == '0138 DG Glut_3') |> 
 ggplot() +
-  aes(x = activity_condition, y = cluster_count, fill = cluster_name) +
-  geom_bar(stat = "identity", position = 'fill', linewidth = 1) +
-  scale_fill_manual(values = cluster_colors) +
-  theme_void() +
-  theme(legend.position = 'none',
-        strip.text = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank())
-si(400, 1200)
+  aes(x = activity_condition, y = cluster_pct, fill = cluster_name) +
+  geom_col(position = 'stack', linewidth = 1) +
+  scale_y_break(c(2, 88)) +
+  scale_fill_manual(values = cluster_colors)
+print(p3)
+
+
+# save ----------------------------------------
+if (SAVE_PLOTS) {
+  # png
+  ggsave(plot = p1,
+         filename = 'excitatory_supertype_by_condition.png',
+         path = save_dir,
+         width = 15, height = 10, dpi = 300, bg = 'white')
+  ggsave(plot = p2,
+         filename = 'excitatory_cluster_by_condition.png',
+         path = save_dir,
+         width = 15, height = 10, dpi = 300, bg = 'white')
+  ggsave(plot = p3,
+         filename = 'DG_Glut3_cluster_by_condition.png',
+         path = save_dir,
+         width = 15, height = 10, dpi = 300, bg = 'white')
+  # svg
+  ggsave(plot = p1 + LoadBarebonesTheme(ticks = 'y'),
+         filename = 'excitatory_supertype_by_condition.svg',
+         path = save_dir,
+         width = 15, height = 10, bg = 'transparent')
+  ggsave(plot = p2 + LoadBarebonesTheme(ticks = 'y'),
+         filename = 'excitatory_cluster_by_condition.svg',
+         path = save_dir,
+         width = 15, height = 10, bg = 'transparent')
+  ggsave(plot = p3 + LoadBarebonesTheme(ticks = 'y'),
+         filename = 'DG_Glut3_cluster_by_condition.svg',
+         path = save_dir,
+         width = 15, height = 10, bg = 'transparent')
+  
+}
+
 
 # tmp color codes for visual cross-ref
-
 # 0136 DG Glut_1 0137 DG Glut_2 0138 DG Glut_3 0139 DG Glut_4 
 c("#F573FF",      "#009BCC",      "#FF7726",      "#FFBF26")
 
