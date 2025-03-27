d8e3d4671b4b863552cc6539dde2b7dcebd03d69
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2025-03-27 16:38:54 -0700

    CRAB num DEG plots

--- 03-scripts/figures/CRAB_numEEorKAGenes.R
+++ 03-scripts/figures/CRAB_numEEorKAGenes.R
@@ -4,13 +4,14 @@ library(readr)
 library(dplyr)
 library(glue)
 library(patchwork)
+library(ggpattern)
 
 source('03-scripts/R/seq_functions.R')
 
-nuclei <- LoadDataset('Dec2024')
+# nuclei <- LoadDataset('Dec2024')
 activity_colors <- LoadActivityColors()
-subclass_colors <- LoadSubclassColors()
-
+subclass_colors <- LoadAllenColors()
+save_dir <- "05-results/CRAB/raw_R_plots"
 
 # subset object and get genes ----------------------------------------
 # A helper function that safely reads a CSV file. If the file doesn't exist,
@@ -25,7 +26,7 @@ safe_read_csv <- function(filepath) {
 }
 
 # Example: load the subclasses to iterate over.
-subclasses_raw <- LoadSubclassesToUse(nuclei)
+subclasses_raw <- LoadSubclassesToUse()
 subclasses <- subclasses_raw[-c(2,8,10:14)] |> print() # dont load small subclasses
 
 df_all <- tibble(df_all <- tibble(
@@ -111,50 +112,85 @@ df_all_processed <- df_all |>
   pivot_longer(
     cols = c(EE_specific, shared, KA_specific),
     names_to = "group",
-    values_to = "count"
+    values_to = "count_raw"
   ) |>
+  # log10 transform counts
+  mutate(count_log = log10(count_raw)) |> 
+  # set log10(0) == -Inf >>> log10(0) == 0
+  mutate(count_log = ifelse(is.infinite(count_log), 0, count_log)) |>
   mutate(
     # for downregulated genes, use negative counts
-    count = ifelse(direction == "down", -count, count),
+    count_log = ifelse(direction == "down", -count_log, count_log),
+    label_y = ifelse(direction == "up", count_log + 0.2, count_log - 0.2),
     time = factor(time, levels = c("30m", "6h")),
     subclass = factor(subclass, levels = (subclasses)),
     group = factor(group, levels = c("KA_specific", "shared", "EE_specific"))
   ) |> 
   print()
 
-df_30m <- df_all_processed |> filter(time == "30m")
-df_6h  <- df_all_processed |> filter(time == "6h")
-
 gene_category_colors <- c(EE_specific = 'gray20', shared = 'gray50', KA_specific = 'gray80')
+direction_patterns <- c(up = 'none', down = 'stripe')
 
 
 # plot num DEGs ------------------------------------------------------
 # 30m plot
-ggplot(df_30m, aes(y = count, x = subclass, fill = group)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), color = 'black') +
+p30m <- df_all_processed |> 
+  filter(time == "30m") |> 
+ggplot() +
+  aes(y = count_log, x = subclass, fill = group) +
+  geom_bar(stat = "identity", , position = position_dodge(width = 0.9), color = 'black') +
+  geom_text(aes(label = count_raw, y = label_y), position = position_dodge(width = 0.9)) +
   geom_hline(yintercept = 0, linetype = "dashed") +
   scale_fill_manual(values = gene_category_colors) +
+  labs(title = 'DEG number by stimuli at 30m', y = 'log10(DEG count)') +
   theme_bw() +
-  theme(legend.position = 'none',
-        legend.title = element_blank(),
-        legend.text = element_text(size = 12),
-        axis.title = element_blank(),
-        axis.text.y = element_text(size = 20),
-        axis.text.x = element_blank(),
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(size = 15, angle = 30, hjust = 1),
+    plot.title= element_text(size = 25)
   )
-si(1000, 500)
+print(p30m)
 
 # 6h plot
-ggplot(df_6h, aes(y = count, x = subclass, fill = group)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), color = 'black') +
+p6h <- df_all_processed |> 
+  filter(time == "6h") |> 
+  ggplot() +
+  aes(y = count_log, x = subclass, fill = group) +
+  geom_bar(stat = "identity", , position = position_dodge(width = 0.9), color = 'black') +
+  geom_text(aes(label = count_raw, y = label_y), position = position_dodge(width = 0.9)) +
   geom_hline(yintercept = 0, linetype = "dashed") +
   scale_fill_manual(values = gene_category_colors) +
+  labs(title = 'DEG number by stimuli at 6h', y = 'log10(DEG count)') +
   theme_bw() +
-  theme(legend.position = 'none',
-        legend.title = element_blank(),
-        legend.text = element_text(size = 12),
-        axis.title = element_blank(),
-        axis.text.y = element_text(size = 20),
-        axis.text.x = element_blank(),
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(size = 15, angle = 30, hjust = 1),
+    plot.title= element_text(size = 25)
   )
-si(1000,500)
+print(p6h)
+
+
+# save --------------------------------------------
+if (SAVE_PLOTS) {
+  h = 8
+  w = 12
+  # png
+  ggsave(p30m,
+         filename = 'numDEG_30m.png',
+         path = save_dir,
+         height = h, width = w)
+  ggsave(p6h,
+         filename = 'numDEG_6h.png',
+         path = save_dir,
+         height = h, width = w)
+  
+  # svg
+  svgsave(p30m + LoadBarebonesTheme(ticks = 'y'),
+          filename = 'numDEG_30m.svg',
+          savedir = save_dir,
+          h = h, w = w)
+  svgsave(p6h + LoadBarebonesTheme(ticks = 'y'),
+          filename = 'numDEG_6h.svg',
+          savedir = save_dir,
+          h = h, w = w)
+}
