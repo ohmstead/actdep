4a7f767bf55107ace29a59fbc23991736746b636
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2025-03-28 10:52:25 -0700

    define DEGs by shrunken LFC

--- 03-scripts/R/manuscript_DGE_pseudobulk.R
+++ 03-scripts/R/manuscript_DGE_pseudobulk.R
@@ -4,60 +4,56 @@ library(tidyverse)
 library(DESeq2)
 
 source("03-scripts/R/seq_functions.R")
-
-nuclei <- LoadDataset("Dec2024")
-
 csv_write_dir <- "04-analysis/DEGs/Dec2024_activity_condition_pseudobulk"
 
-
-# findDEG() ----
-findDEG <- function(seurat_obj, subclass, group1, group2, logFC_threshold=0.25, pct = 0.01) {
-  # print report statement
-  print(glue("{subclass} between {group1} and {group2}."))
-
-  # skip is number of cells < 30 per group
-  if (sum(seurat_obj$activity_condition == group1) < 30 | 
-      sum(seurat_obj$activity_condition == group2) < 30) {
-    print(glue("Skipping {subclass} between {group1} and {group2} due to low cell count."))
-
-    return(data.frame())
-  }
-  
-  # extract the contrast
-  
-  return(DEGs)
-}
-
-getConstrastResults <- function(dds, contrast, threshold) {
-  res <- results(dds, contrast = contrast) |> 
-    as.data.frame() |> 
-    rownames_to_column("gene") |> 
-    relocate(gene) |> 
-    mutate(
-      classification = 
-        ifelse(log2FoldChange > threshold & padj < 0.05, "upregulated", 
-               ifelse(log2FoldChange < -threshold & padj < 0.05, "downregulated", 
-                      "no_change")
-        )
-    ) |> 
-    arrange(padj)
-    
-    print(tibble(res))
-
-  rownames(res) <- res$gene
-  
-  return(tibble(res))
-}
-
-
-# run DEG analysis for each subclass ----
+nuclei <- LoadDataset("Dec2024")
+log2FC_threshold <- 0.585  # corresponding to 1.5 fold-change (i.e. 50%)
 subclass_list <- LoadSubclassesToUse(nuclei)
 
-log2FC_threshold <- 0.585  # corresponding to 50% change
 
-for (i in seq_along(subclass_list)) {
-  subclass <- subclass_list[i]
-  print(glue("subclass {i}/{length(subclass_list)} -- {subclass}"))
+getConstrastResults <- function(dds, contrast_name, threshold) {
+  # get results from Wald test
+  res_raw <- results(dds, name = contrast_name)
+  
+  # filter by shrunken lfc
+  res_shrink <- lfcShrink(dds = dds, 
+                          res = res_raw, 
+                          coef = contrast_name, 
+                          type = 'apeglm')|> 
+    as_tibble(rownames = 'gene') |> 
+    left_join(select(as_tibble(res_raw, rownames = 'gene'), gene, log2FoldChange, lfcSE), 
+              by = 'gene',
+              suffix = c('.shrink', '.raw'),
+              relationship = 'one-to-one') |> 
+    arrange(padj) |> 
+    relocate(log2FoldChange.raw, log2FoldChange.shrink, lfcSE.raw, lfcSE.shrink, .after = baseMean) |>
+    mutate(
+      classification = 
+        ifelse(  # if upregulated
+          log2FoldChange.shrink > threshold & padj < 0.05, 
+          "upregulated", 
+          ifelse(  # if downregulated
+            log2FoldChange.shrink < -threshold & padj < 0.05,
+            "downregulated", 
+            "no_change" # then no change
+          )
+        )
+    )
+  print(res_shrink)
+  return(res_shrink)
+}
+
+
+# run DEG analysis for each subclass --------------------------------------------
+# set up progress bar
+pb <- progress_bar$new(
+  format = glue("  :subclass_name [:bar] :current/:total (:percent) in :elapsed"),
+  total = length(subclass_list), clear = FALSE, width= 60)
+pb$tick(0) # init progress bar
+
+for (subclass in subclass_list) {
+  # report programm
+  pb$tick(tokens = list(subclass_name = subclass))
 
   # subset nuclei for current subclass
   nuclei_subclass <- subset(nuclei, subclass_name == subclass)
@@ -96,21 +92,17 @@ for (i in seq_along(subclass_list)) {
   dds <- DESeq(dds)
   
   # get contrasts available for this subclass
-  df_contrasts <- GetSubclassContrasts(nuclei, subclass, cell_cutoff = 30)
+  df_contrasts <- GetSubclassContrasts(nuclei, subclass, cell_cutoff = 30) |> 
+    filter(group2 == 'SE')
   
   # run contrasts for cell groups that meet cell_cutoff
   for (k in seq_along(df_contrasts$group1)) {
     group1 <- as.character(df_contrasts$group1[k])
     group2 <- as.character(df_contrasts$group2[k])
     
-    specific_contrast <- c("activity_condition", group1, group2)
+    contrast_to_test <- glue('activity_condition_{group1}_vs_{group2}')
 
-    DGE_results <- getConstrastResults(dds, contrast = specific_contrast, log2FC_threshold)
-    df_shrink <- QuickPercentExpression(nuclei_subclass, DGE_results$gene, group1, group2, assay = "SCT", slot = "data")
-    DGE_results <- DGE_results |> 
-      left_join(df_shrink, by = "gene") |> 
-      mutate(log2FoldChange.shrink = log2FoldChange * shrink_coef) |> 
-      relocate(gene, log2FoldChange, log2FoldChange.shrink, padj, classification)
+    DGE_results <- getConstrastResults(dds, contrast = contrast_to_test, log2FC_threshold)
     write_csv(DGE_results, glue("{csv_write_dir}/{subclass_fname}__{group1}_vs_{group2}.csv"))
   }
 }
