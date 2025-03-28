699798138f961b2987e8c518f5861212078a9560
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2025-03-28 10:51:28 -0700

    add ZT to ZUPP_NEWT_QC

--- 03-scripts/figures/ZUPP_NEWT_qc.R
+++ 03-scripts/figures/ZUPP_NEWT_qc.R
@@ -1,35 +1,18 @@
-library(tidyverse)
-library(ggplot2)
-library(patchwork)
 library(paletteer)
 library(Seurat)
-library(SeuratDisk)
 
 source("03-scripts/R/seq_functions.R")
 
 # load colors  ----------------------------------------------------------------------
 nuclei <- LoadDataset("Dec2024")
 
-activity_colors <- LoadActivityColors('Dec2024')
+class_colors <- LoadAllenColors('class')
 subclass_colors <- LoadAllenColors('subclass')
-ZT_colors <- LoadZTColors(5)
+activity_colors <- LoadActivityColors()
+ZT_colors <- LoadZTColors()
 sex_colors <- LoadSexColors()
 
-save_path <- "05-results/NEWT_ZUPP_QC/raw_R_plots/"
-
-# plot Xist expression ----------------------------------------------------------------------
-# let's ascertain the sex of biological samples based on Xist expression
-nuclei <- nuclei |> 
-  AddMetaData(metadata = nuclei@assays$RNA$counts['Xist',], col = 'Xist')
-
-meta <- nuclei@meta.data
-
-# plot Xist expression in each sample
-ggplot(meta) +
-  geom_histogram(aes(x = Xist, fill = activity_condition)) +
-  labs(title = 'Xist expression') +
-  facet_wrap(~sample, nrow = 4) +
-  scale_fill_manual(values = activity_colors)
+save_path <- "05-results/ZUPP_NEWT_QC/raw_R_plots"
 
 
 # relevel, filter subclasses ----------------------------------------------------------------------
@@ -43,9 +26,11 @@ meta <- meta |>
   mutate(subclass_name = fct_reorder(subclass_name, count, .desc = FALSE)) |> 
   select(-count)
 
-# plot ----------------------------------------------------------------------
+# qc subclass ----------------------------------------------------------------------
 my_theme <- theme(
-  # text = element_text(family = 'Mono'),
+  axis.title.y = element_blank(),
+  axis.text.y = element_blank(),
+  axis.text.x = element_text(angle=30, hjust=1),
   legend.position = 'none',
   plot.margin = margin(0, 0, 0, 0),
   panel.background = element_blank(),
@@ -60,13 +45,11 @@ pNum <- ggplot(meta) +
   geom_bar(aes(fill = subclass_name)) + 
   scale_fill_manual(values = subclass_colors) +
   scale_x_log10(breaks = c(100, 1000, 10000), position = 'bottom') +
-  labs(x = '', y = '') +
+  labs(x = 'num cells') +
   my_theme +
   theme(
-    axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
-    # axis.text.y = element_text(size = 18),
-    axis.text.y = element_blank(),
-    axis.text.x = element_blank(),
+    axis.text.y = element_text(size = 15),
+    axis.title.x = element_text(size = 15),
   )
 
 # condition ratios
@@ -77,15 +60,9 @@ ggplot() +
     aes(y = subclass_name, x = n, fill = activity_condition) +
     geom_bar(stat = 'identity', position = 'fill') +
     scale_fill_manual(values = activity_colors) +
-    scale_x_continuous(breaks = NULL, position = 'top') +
-    labs(y = '', x = '') +
-    my_theme +
-    theme(
-      axis.text.y = element_blank(),
-      axis.text.x = element_text(angle = 270, hjust = 1),
-      axis.ticks.x = element_blank(),
-      axis.ticks.y = element_blank(),
-    )
+    scale_x_continuous(breaks = NULL, position = 'bottom') +
+    labs(x = 'activity') +
+    my_theme
 
 # sex ratios
 pSex <- meta |> 
@@ -94,16 +71,10 @@ pSex <- meta |>
 ggplot() +
     aes(y = subclass_name, x = n, fill = sex) +
     geom_bar(stat = 'identity', position = 'fill') +
-    scale_x_continuous(breaks = NULL, position = 'top') +
+    scale_x_continuous(breaks = NULL, position = 'bottom') +
     scale_fill_manual(values = sex_colors) +
-    labs(y = '', x = '') +
-    my_theme +
-    theme(
-      axis.text.y = element_blank(),
-      axis.text.x = element_text(angle = 270, hjust = 1),
-      axis.ticks.x = element_blank(),
-      axis.ticks.y = element_blank(),
-    )
+    labs(x = 'sex') +
+    my_theme
 
 # sublibrary ratio
 pSublib <- meta |> 
@@ -112,16 +83,10 @@ pSublib <- meta |>
 ggplot() +
     aes(y = subclass_name, x = n, fill = sublibrary) +
     geom_bar(stat = 'identity', position = 'fill') +
-    scale_x_continuous(breaks = NULL, position = 'top') +
-    scale_fill_paletteer_d("ltc::reading") + 
-    labs(y = '', x = '') +
-    my_theme +
-    theme(
-      axis.text.y = element_blank(),
-      axis.text.x = element_text(angle = 270, hjust = 1),
-      axis.ticks.x = element_blank(),
-      axis.ticks.y = element_blank(),
-    )
+    paletteer::scale_fill_paletteer_d("ltc::reading") + 
+    scale_x_continuous(breaks = NULL, position = 'bottom') +
+    labs(x = 'sublib') +
+    my_theme
 
 # ZT ratio
 pZT <- meta |> 
@@ -130,16 +95,10 @@ pZT <- meta |>
 ggplot() +
     aes(y = subclass_name, x = n, fill = ZT) +
     geom_bar(stat = 'identity', position = 'fill') +
-    scale_x_continuous(breaks = NULL, position = 'top') +
+    scale_x_continuous(breaks = NULL, position = 'bottom') +
     scale_fill_manual(values = ZT_colors) +
-    labs(y = '', x = 'ZT') +
-    my_theme +
-    theme(
-      axis.text.y = element_blank(),
-      axis.text.x = element_text(angle = 270, hjust = 1),
-      axis.ticks.x = element_blank(),
-      axis.ticks.y = element_blank(),
-    )
+    labs(x = 'ZT') +
+    my_theme
 
 # UMI plot
 pUMI <- ggplot(meta) +
@@ -148,14 +107,9 @@ pUMI <- ggplot(meta) +
   geom_violin(aes(fill = subclass_name), width = 2) +
   scale_fill_manual(values = subclass_colors) +
   geom_boxplot(fill = 'gray80', width = 0.3, outliers = FALSE) +
-  scale_x_log10(position = 'top') +
-  labs(y = '', x = 'UMI count') +
-  my_theme +
-  theme(
-    axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-  )
+  scale_x_log10(position = 'bottom') +
+  labs(x='UMI') +
+  my_theme
 
 # gene plot
 pGenes <- ggplot(meta) +
@@ -163,14 +117,9 @@ pGenes <- ggplot(meta) +
   geom_violin(aes(fill = subclass_name), width = 2) +
   scale_fill_manual(values = subclass_colors) +
   geom_boxplot(fill = 'gray80', width = 0.3, outliers = FALSE) +
-  scale_x_continuous(position = 'top') +
-  labs(y = '', x = 'gene count') +
-  my_theme +
-  theme(
-    axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-  )
+  scale_x_continuous(position = 'bottom') +
+  labs(x = 'genes') +
+  my_theme
 
 # pct.mt plot
 pMt <- ggplot(meta) +
@@ -178,21 +127,30 @@ pMt <- ggplot(meta) +
   geom_violin(aes(fill = subclass_name), width = 2) +
   scale_fill_manual(values = subclass_colors) +
   geom_boxplot(fill = 'gray80', width = 0.3, outliers = FALSE) +
-  scale_x_continuous(position = 'top') +
+  scale_x_continuous(position = 'bottom') +
   labs(y = '', x = '% mt reads') +
-  my_theme +
-  theme(
-    axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-  )
+  my_theme
 
-pNum + pActivity + pSex + pSublib + plot_layout(widths = c(30, 3, 3, 3))
-ggsave('quality_by_subclass_short.png', save_path, width = 15, height = 10, dpi = 600)
-ggsave('quality_by_subclass_short.svg', save_path, width = 15, height = 10)
+p <- pNum + pActivity + pZT + pSex + pSublib + plot_layout(widths = c(30, 3, 3, 3, 3))
+print(p)
 
-pNum + pActivity + pSex + pSublib + pZT + pUMI + pGenes + pMt + plot_layout(widths = c(5, 3, 3, 3, 3, 25, 25, 25, 10))
-ggsave('quality_by_subclass.png', save_path, width = 27, height = 9, dpi = 600)
+# subclass QC for figure
+ggsave(plot = p,  # PNG
+       filename = 'quality_by_subclass_short.png', 
+       path = save_path, 
+       width = 15, height = 10, dpi = 300)
+ggsave(plot = p *LoadBarebonesTheme(ticks = 'x'),  # SVG
+       filename = 'quality_by_subclass_short.svg', 
+       path = save_path, 
+       width = 15, height = 10, dpi = 300)
+
+p <- pNum + pUMI + pGenes + pMt + pActivity + pSex + pSublib + pZT + 
+  plot_layout(widths = c(10, 10, 10, 10, 3, 3, 3, 3, 3))
+print(p)
+ggsave(plot = p,  # PNG
+       filename = 'quality_by_subclass.png',
+       path = save_path,
+       width = 27, height = 9, dpi = 600)
 
 
 ## ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -241,24 +199,24 @@ pSex <- meta |>
     )
 
 # mread plot
-pReads <- ggplot(meta) +
-  aes(y = sample, x = mread_count, fill = activity_condition) +
-  geom_violin(width = 2) +
-  geom_boxplot(width = 0.2, outliers = FALSE) +
-  scale_fill_manual(values = activity_colors) +
-  scale_x_log10(position = 'top') +
-  scale_y_discrete(limits = rev) +
-  labs(y = '', x = 'read count') +
-  my_theme +
-  theme(
-    axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-  )
+# pReads <- ggplot(meta) +
+#   aes(y = sample, x = mread_count, fill = activity_condition) +
+#   geom_violin(width = 2) +
+#   geom_boxplot(width = 0.2, outliers = FALSE) +
+#   scale_fill_manual(values = activity_colors) +
+#   scale_x_log10(position = 'top') +
+#   scale_y_discrete(limits = rev) +
+#   labs(y = '', x = 'read count') +
+#   my_theme +
+#   theme(
+#     axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
+#     axis.text.y = element_blank(),
+#     axis.ticks.y = element_blank(),
+#   )
 
 # UMI plot
 pUMI <- ggplot(meta) +
-  aes(y = sample, x = tscp_count, fill = activity_condition) +
+  aes(y = sample, x = nCount_RNA, fill = activity_condition) +
   geom_violin(width = 2) +
   geom_boxplot(width = 0.2, outliers = FALSE) +
   scale_fill_manual(values = activity_colors) +
@@ -274,7 +232,7 @@ pUMI <- ggplot(meta) +
 
 # gene plot
 pGenes <- ggplot(meta) +
-  aes(y = sample, x = gene_count, fill = activity_condition) +
+  aes(y = sample, x = nFeature_RNA, fill = activity_condition) +
   geom_violin(width = 2) +
   geom_boxplot(width = 0.2, outliers = FALSE) +
   scale_fill_manual(values = activity_colors) +
@@ -303,57 +261,22 @@ pMt <- ggplot(meta) +
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank(),
   )
+p <- pNum + pSex + pUMI + pGenes + pMt + plot_layout(widths = c(5, 3, 25, 25, 10), nrow = 1)
+print(p)
 
-pNum + pSex + pReads + pUMI + pGenes + pMt + plot_layout(widths = c(5, 3, 25, 25, 25, 10), nrow = 1)
-
-ggsave('quality_by_sample.png', save_path, width = 27, height = 14)
-ggsave('quality_by_sample.svg', save_path, width = 27, height = 14)
-
-
-# show disto for each class ----------------------------------------------------------------------
-class_colors <- LoadAllenColors('class')
-
-meta |> 
-  filter(ZT == 'ZT4') |> 
-ggplot() +
-  aes(y = interaction(class_name, sample), x = gene_count, fill = class_name) +
-  # geom_violin(width = 2) +
-  geom_boxplot(width = 0.2, outliers = FALSE) +
-  scale_x_log10(position = 'top') +
-  scale_y_discrete(limits = rev) +
-  labs(y = '', x = 'gene count') +
-  my_theme +
-  theme(
-    axis.text.x.top = element_text(angle = 270, hjust = 1, vjust = 0.5),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank()
-  )
+ggsave(plot = p,
+       filename = 'quality_by_sample.png', 
+       path = save_path, 
+       width = 27, height = 14)
 
 
-# distro for reads ----------------------------------------------------------------------
-# plot reads by class
-pReads <- ggplot(meta) +
-  aes(x = sample, y = mread_count, fill = class_name) +
-  geom_violin() +
-  geom_boxplot(width = 0.2, outliers = FALSE) +
-  scale_y_log10(labels = scales::label_comma()) +
-  scale_x_discrete() +
-  facet_wrap(~class_name, 
-             nrow = length(unique(meta$class_name)),
-             scales = 'free_y') +
-  my_theme + 
-  theme(strip.text = element_text(size = 25),
-        axis.text.x = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank())
-
-# plot for n cells
+# plot sample N ----------------------------------------------------------------------
 pN <- meta |> 
   group_by(sample) |> 
   summarize(n = n(),
             ZT = first(ZT),
             activity_condition = first(activity_condition)) |> 
-ggplot() +
+  ggplot() +
   aes(x = sample, y = n) +
   geom_col() +
   scale_y_log10(breaks = c(100, 1000, 2000, 3000),
@@ -367,15 +290,35 @@ ggplot() +
     axis.text.x = element_text(size = 15, angle = 90, hjust = 1)
   )
 
-pReads / pN + plot_layout(heights = c(10, 1))
 
-ggsave('reads_by_class.png', save_path, width = 9, height = 18, dpi = 600)
+# # distro for reads ----------------------------------------------------------------------
+# # plot reads by class
+# pReads <- ggplot(meta) +
+#   aes(x = sample, y = mread_count, fill = class_name) +
+#   geom_violin() +
+#   geom_boxplot(width = 0.2, outliers = FALSE) +
+#   scale_y_log10(labels = scales::label_comma()) +
+#   scale_x_discrete() +
+#   facet_wrap(~class_name, 
+#              nrow = length(unique(meta$class_name)),
+#              scales = 'free_y') +
+#   my_theme + 
+#   theme(strip.text = element_text(size = 25),
+#         axis.text.x = element_blank(),
+#         axis.title.x = element_blank(),
+#         axis.title.y = element_blank())
+# 
+# pReads <- pReads / pN + plot_layout(heights = c(10, 1))
+# print(pReads)
+# ggsave('class_reads.png', 
+#        path = save_path,
+#        width = 9, height = 18, dpi = 600)
 
 
 # distro for UMIs ----------------------------------------------------------------------
 # plot reads by class
 pUMI <- ggplot(meta) +
-  aes(x = sample, y = tscp_count, fill = class_name) +
+  aes(x = sample, y = nCount_RNA, fill = class_name) +
   geom_violin() +
   geom_boxplot(width = 0.2, outliers = FALSE) +
   scale_y_log10(labels = scales::label_comma()) +
@@ -384,20 +327,23 @@ pUMI <- ggplot(meta) +
              nrow = length(unique(meta$class_name)),
              scales = 'free_y') +
   my_theme + 
-  theme(strip.text = element_text(size = 25),
+  theme(strip.text = element_text(size = 12),
         axis.text.x = element_blank(),
         axis.title.x = element_blank(),
         axis.title.y = element_blank())
 
-pUMI / pN + plot_layout(heights = c(10, 1))
-
-ggsave('tscp_by_class.png', save_path, width = 9, height = 18, dpi = 600)
+pUMI <- pUMI / pN + plot_layout(heights = c(10, 1))
+print(pUMI)
+ggsave(plot = pUMI,
+       'class_UMI.png', 
+       path = save_path, 
+       width = 9, height = 18, dpi = 600)
 
 
 # distro for genes ----------------------------------------------------------------------
 # plot reads by class
 pGenes <- ggplot(meta) +
-  aes(x = sample, y = gene_count, fill = class_name) +
+  aes(x = sample, y = nFeature_RNA, fill = class_name) +
   geom_violin() +
   geom_boxplot(width = 0.2, outliers = FALSE) +
   scale_y_log10(labels = scales::label_comma()) +
@@ -406,12 +352,14 @@ pGenes <- ggplot(meta) +
              nrow = length(unique(meta$class_name)),
              scales = 'free_y') +
   my_theme + 
-  theme(strip.text = element_text(size = 25),
+  theme(strip.text = element_text(size = 12),
         axis.text.x = element_blank(),
         axis.title.x = element_blank(),
         axis.title.y = element_blank())
 
-pGenes / pN + plot_layout(heights = c(10, 1))
-
-ggsave('genes_by_class.png', save_path, width = 9, height = 18, dpi = 600)
-
+pGenes <- pGenes / pN + plot_layout(heights = c(10, 1))
+print(pGenes)
+ggsave(plot = pGenes,
+       'class_genes.png', 
+       path = save_path,
+       width = 9, height = 18, dpi = 600)
