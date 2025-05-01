3bec68d2b8a85d28020774853977fc324cb2617f
jack olmstead <20440639+ohmstead@users.noreply.github.com>
2025-05-01 10:14:58 -0700

    update CROW heatmap code

--- 03-scripts/figures/CROW_heatmap_tau_gigaclass.R
+++ 03-scripts/figures/CROW_heatmap_tau_gigaclass.R
@@ -1,39 +1,39 @@
-library(ggplot2)
-library(dplyr)
-library(tidyr)
-
-library(Seurat)
+# Script to plot heatmaps from figure 1, with gene columns organized by tau.
 library(ComplexHeatmap)
 
 source('03-scripts/R/seq_functions.R')
+
+gigaclasses <- LoadSubclassesToUse(nuclei, as_gigaclasses = T)
 activity_colors <- LoadActivityColors("Dec2024")
 subclass_colors <- LoadAllenColors("subclass")
 
 
-# establish subclasses to use ----
-print('Subsetting Seurat object...')
-subclass_sets <- LoadSubclassesToUse(nuclei, as_gigaclasses = T)
-seurat_subsets <- LoadDataset("Dec2024", as_gigaclasses = T)
+# establish subclasses to use ----------------------------------------
 
-ieg_symbols <- c(LoadGeneList('IEG'), 'Rn7sk', 'Rcan2', 'Tac1', 'Map3k19')
+ieg_symbols <- c(LoadGeneList('IEG'), 'Rn7sk', 'Midn', 
+                 'Etv1', 'Rcan2', 'Hs3st2',
+                 'Tac1', 'Socs2', 'Daam2', 'Akap5', 'Frmd6', 'Crhbp', 'Gm49673',
+                 'Map3k19', 'Usp53', 'Slco1c1', 'Kcnn2')
 
 # loop through cell superclasses
 plots_tau <- list()
-for (set_name in names(seurat_subsets)) {
-  subclass_list <- subclass_sets[[set_name]]
-  nuclei_subclass <- seurat_subsets[[set_name]]
-  nuclei_subclass$subclass_name |> table()
-  df_expression <- read_csv(glue("04-analysis/df_expression/df_expression.seurat/df_expression_{set_name}.csv")) |> 
-    print()
+for (gigaclass_str in names(gigaclasses)) {
+  subclass_list <- gigaclasses[[gigaclass_str]]
   
+  # load df_expression
+  load_dir <- "04-analysis/DEGs/Dec2024_activity_condition_pseudobulk"
+  fpath <- glue("{load_dir}/0_df_expression_{gigaclass_str}.csv")
+  df_expression <- read_csv(fpath, show_col_types = F)
+  
+  # calculate tau
   df_sub <- df_expression |>
     filter(activity_condition == 'EE30m',
            classification == 'ERG',
            direction == 'up'
            ) |>
     group_by(gene) |> 
-    mutate(tau = tau(log2FoldChange.shrink, direction = 'up')) |>
-    select(gene, subclass, log2FoldChange.shrink, tau)
+    mutate(tau = tau(log2FC_calculated, direction = 'up')) |>
+    select(gene, subclass, log2FC_calculated, tau)
   
   gene_tau <- df_sub |> 
     group_by(gene) |> 
@@ -44,7 +44,7 @@ for (set_name in names(seurat_subsets)) {
   # Pivot to wide format so that rows are subclasses and columns are genes.
   mat <- df_sub |>
     select(-tau) |> 
-    pivot_wider(names_from = gene, values_from = log2FoldChange.shrink) |>
+    pivot_wider(names_from = gene, values_from = log2FC_calculated) |>
     column_to_rownames("subclass") |>
     as.matrix()
   
@@ -58,7 +58,7 @@ for (set_name in names(seurat_subsets)) {
   # Order genes by tau (highest to lowest)
   gene_order <- names(sort(gene_tau, decreasing = TRUE))
   bar_colors <- ifelse(gene_tau[gene_order] > 0.8, "red", "gray60")
-  mat_ordered <- mat[, gene_order, drop = FALSE]
+  mat_ordered <- mat[, gene_order, drop = FALSE]  # set gene cols
   
   # (Optional) Create a top annotation showing tau as a barplot
   top_anno <- HeatmapAnnotation(
@@ -79,18 +79,40 @@ for (set_name in names(seurat_subsets)) {
   # plot ----------------------------------------------------------------
   p <- Heatmap(mat_ordered,
                name = "log2FC",
-               col = circlize::colorRamp2(c(-2, 0, 2), hcl_palette = 'Blue-Red 2'),
+               col = circlize::colorRamp2(c(-3, 0, 3), hcl_palette = 'RdBu', reverse = T),
+               row_order = subclass_list,
                top_annotation = top_anno,
                bottom_annotation = bottom_anno,
-               cluster_rows = FALSE,
-               cluster_columns = FALSE,
-               show_row_names = FALSE,
-               show_column_names = FALSE,
-               # column_title = paste(set_name, 'ERG', "tau-organized heatmap"),
-               heatmap_legend_param = list(title = "log2FC"))
+               height = unit(7.5*length(subclass_list), 'mm'),
+               cluster_rows = F,
+               cluster_columns = F,
+               show_row_names = T,
+               show_column_names = F
+  )
   draw(p, heatmap_legend_side = "bottom")
   
-  plots_tau[[paste(set_name, 'ERG', sep = "_")]] <- p
+  plots_tau[[paste(gigaclass_str, 'ERG', sep = "_")]] <- p
 }
 
-InteractiveComplexHeatmap::htShiny(plots_tau[[1]])
+InteractiveComplexHeatmap::htShiny(plots_tau[[2]])
+
+
+# save ----------------------------------------
+if (SAVE_PLOTS){
+  save_dir <- "05-results/CROW/raw_R_plots"
+  for (current_plot in names(plots_tau)) {
+    p <- plots_tau[[current_plot]]
+    plot_path <- glue("{save_dir}/PNG_{current_plot}.png")
+    
+    # png
+    png(plot_path, width = 8, height = 5, units = "in", res = 900)
+    draw(p, heatmap_legend_side = 'bottom')
+    dev.off()
+    
+    # svg
+    svgsave(plot = p,
+            filename = glue("SVG_{current_plot}.svg"), 
+            path = save_dir, 
+            width = 8, height = 5)
+  }
+}
